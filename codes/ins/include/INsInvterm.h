12de1c817e1f2162fc40902d188aea4e340017c6
ZJZT800 <57002932+ZJZT800@users.noreply.github.com>
2020-03-27 21:52:57 +0800

    1

--- codes/ins/include/INsInvterm.h
+++ codes/ins/include/INsInvterm.h
@@ -70,7 +70,7 @@ public:
     Real eig11, eig12, eig13;
     Real eig21, eig22, eig23;
 
-	Real vnrel, vnflow, fq0, clr, crl, ai1, ai2, Vau, Vav, Vaw,pp, ;
+	Real vnrel, vnflow, fq0, clr, crl, ai1, ai2, Vau, Vav, Vaw,pp;
     Real cl, cr, cm;
 public:
     Real rl, ul, vl, wl, pl, hl, el;

5df8fd77cad32a88fdab56d60b2edf0a2c0b9700
ZJZT800 <57002932+ZJZT800@users.noreply.github.com>
2020-03-28 16:06:19 +0800

    1

--- codes/uins/src/UINsVisterm.cpp
+++ codes/uins/src/UINsVisterm.cpp
@@ -157,9 +157,9 @@ void UINsVisterm::CmpSrc()
 		ug.rc = (*ug.rcf)[ug.fId];
 		//if ( ug.lc == 0 ) cout << fId << endl;
 
-			iinv.spu1[ug.lc]+= iinv.ai1[ug.lc]+ iinv.rl * gcom.cvol1 / (*uinsf.timestep)[0][ug.lc] - gcom.cvol1 * visQ.dqdx1[IIDX::IIP];
-			iinv.spv1[ug.lc]+= iinv.ai1[ug.lc] + iinv.rl * gcom.cvol1 / (*uinsf.timestep)[0][ug.lc] - gcom.cvol1 * visQ.dqdy1[IIDX::IIP];
-			iinv.spw1[ug.lc]+= iinv.ai1[ug.lc] + iinv.rl * gcom.cvol1 / (*uinsf.timestep)[0][ug.lc] - gcom.cvol1 * visQ.dqdz1[IIDX::IIP];
+			iinv.spu1[ug.lc]+= iinv.ai1[ug.lc]+iinv.bi1[ug.lc]+ iinv.rl * gcom.cvol1 / (*uinsf.timestep)[0][ug.lc] - gcom.cvol1 * visQ.dqdx1[IIDX::IIP];
+			iinv.spv1[ug.lc]+= iinv.ai1[ug.lc]+iinv.bi1[ug.lc]+ iinv.rl * gcom.cvol1 / (*uinsf.timestep)[0][ug.lc] - gcom.cvol1 * visQ.dqdy1[IIDX::IIP];
+			iinv.spw1[ug.lc]+= iinv.ai1[ug.lc] +iinv.bi1[ug.lc] + iinv.rl * gcom.cvol1 / (*uinsf.timestep)[0][ug.lc] - gcom.cvol1 * visQ.dqdz1[IIDX::IIP];
 	}
 
 	for (int fId = ug.nBFace; fId < ug.nFace; ++fId)

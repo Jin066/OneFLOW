cb986caf49635a8e7be979104d58bd910bad8c4d
ZJZT800 <57002932+ZJZT800@users.noreply.github.com>
2020-03-28 09:04:19 +0800

    1

--- codes/ins/src/INsInvterm.cpp
+++ codes/ins/src/INsInvterm.cpp
@@ -112,7 +112,7 @@ void INsInvterm::CmpINsinvTerm()
 	//Real v2 = ONEFLOW::SQR(iinv.um, iinv.vm, iinv.wm);
 
 
-	iinv.vnflow[ug.fId] = gcom.xfn * iinv.um + gcom.yfn * iinv.vm + gcom.zfn * iinv.wm;  //初始界面上 V*n(改动)
+	iinv.vnflow = gcom.xfn * iinv.um + gcom.yfn * iinv.vm + gcom.zfn * iinv.wm;  //初始界面上 V*n(改动)
 
 	//iinv.fq0[ug.fId] = iinv.rl * iinv.vnflow * gcom.farea; //初始界面上的质量通量
 
